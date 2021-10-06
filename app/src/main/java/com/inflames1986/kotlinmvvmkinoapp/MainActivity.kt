a5a2c997e3e1f50c20d93bc8077184640215b67c
TMichael <js5859168@gmail.com>
2021-10-06 18:32:13 +0300

    Starts Kotlin with mvvm Kino App develop

---
+++ app/src/main/java/com/inflames1986/kotlinmvvmkinoapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.inflames1986.kotlinmvvmkinoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inflames1986.kotlinmvvmkinoapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/inflames1986/kotlinmvvmkinoapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.inflames1986.kotlinmvvmkinoapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
